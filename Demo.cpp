7089653bb8673403a9585bdb0b41697652662eba
ofekats <118768684+ofekats@users.noreply.github.com>
2023-06-18 12:09:19 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -42,5 +42,119 @@ int main() {
     container.removeElement(9);
     std::cout << "Size of container after removing an element: " << container.size() << std::endl;
 
+
+
+    cout << "===========================" << endl;
+    cout << "         my demo:" << endl;
+    cout << "===========================" << endl;
+
+    //create MagicalContainer
+    MagicalContainer container2;
+    //print size - should be 0
+    cout << "the container size: " << container2.size() << endl;
+    //add elements
+    container2.addElement(17);
+    //the same element twice
+    container2.addElement(17);
+    //print size - should be 1
+    cout << "the container size: " << container2.size() << endl;
+    //remove element
+    container2.removeElement(17);
+    //print size - should be 0
+    cout << "the container size: " << container2.size() << endl;
+    container2.addElement(11);
+    //AscendingIterator
+    cout << "\n\nAscendingIterator: " << endl;
+
+    //constructor
+    MagicalContainer::AscendingIterator asc_iter1(container2);
+    //copy constructor
+    MagicalContainer::AscendingIterator asc_iter2(asc_iter1);
+    //should be true - 1
+    cout << "(asc_iter1 == asc_iter2): " << (asc_iter1 == asc_iter2) << endl;
+    //Assignment operator
+    MagicalContainer::AscendingIterator asc_iter3 = asc_iter1;
+    //should be true - 1
+    cout << "(asc_iter1 == asc_iter3): " << (asc_iter1 == asc_iter3) << endl;
+    //operator!= - should be false -0
+    cout << "(asc_iter1 != asc_iter3): " << (asc_iter1 != asc_iter3) << endl;
+    //operator++
+    ++asc_iter1;
+    //operator> - should be true - 1
+    cout << "(asc_iter1 > asc_iter3): " << (asc_iter1 > asc_iter3) << endl;
+    //operator< - should be false -0
+    cout << "(asc_iter1 < asc_iter3): " << (asc_iter1 < asc_iter3) << endl;
+    //operator*
+    cout << "*asc_iter2: " << *asc_iter2 << endl;
+    //begin
+    MagicalContainer::AscendingIterator asc_iter4 = asc_iter1.begin();
+    cout << "*asc_iter4: " << *asc_iter4 << endl;
+    //end
+    MagicalContainer::AscendingIterator asc_iter5 = asc_iter4.end();
+    //should be true - 1
+    cout << "(asc_iter4 != asc_iter5): " << (asc_iter4 != asc_iter5) << endl;
+
+    //SideCrossIterator
+    cout << "\n\nSideCrossIterator: " << endl;
+
+    //constructor
+    MagicalContainer::SideCrossIterator side_iter1(container2);
+    //copy constructor
+    MagicalContainer::SideCrossIterator side_iter2(side_iter1);
+    //should be true - 1
+    cout << "(side_iter1 == side_iter2): " << (side_iter1 == side_iter2) << endl;
+    //Assignment operator
+    MagicalContainer::SideCrossIterator side_iter3 = side_iter1;
+    //should be true - 1
+    cout << "(side_iter1 == side_iter3): " << (side_iter1 == side_iter3) << endl;
+    //operator!= - should be false -0
+    cout << "(side_iter1 != side_iter3): " << (side_iter1 != side_iter3) << endl;
+    //operator++
+    ++side_iter1;
+    //operator> - should be true - 1
+    cout << "(side_iter1 > side_iter3): " << (side_iter1 > side_iter3) << endl;
+    //operator< - should be false -0
+    cout << "(side_iter1 < side_iter3): " << (side_iter1 < side_iter3) << endl;
+    //operator*
+    cout << "*side_iter2: " << *side_iter2 << endl;
+    //begin
+    MagicalContainer::SideCrossIterator side_iter4 = side_iter1.begin();
+    cout << "*side_iter4: " << *side_iter4 << endl;
+    //end
+    MagicalContainer::SideCrossIterator side_iter5 = side_iter4.end();
+    //should be true - 1
+    cout << "(side_iter4 != side_iter5): " << (side_iter4 != side_iter5) << endl;
+
+    //PrimeIterator
+    cout << "\n\nPrimeIterator: " << endl;
+
+    //constructor
+    MagicalContainer::PrimeIterator prime_iter1(container2);
+    //copy constructor
+    MagicalContainer::PrimeIterator prime_iter2(prime_iter1);
+    //should be true - 1
+    cout << "(prime_iter1 == prime_iter2): " << (prime_iter1 == prime_iter2) << endl;
+    //Assignment operator
+    MagicalContainer::PrimeIterator prime_iter3 = prime_iter1;
+    //should be true - 1
+    cout << "(prime_iter1 == prime_iter3): " << (prime_iter1 == prime_iter3) << endl;
+    //operator!= - should be false -0
+    cout << "(prime_iter1 != prime_iter3): " << (prime_iter1 != prime_iter3) << endl;
+    //operator++
+    ++prime_iter1;
+    //operator> - should be true - 1
+    cout << "(prime_iter1 > prime_iter3): " << (prime_iter1 > prime_iter3) << endl;
+    //operator< - should be false -0
+    cout << "(prime_iter1 < prime_iter3): " << (prime_iter1 < prime_iter3) << endl;
+    //operator*
+    cout << "*prime_iter2: " << *prime_iter2 << endl;
+    //begin
+    MagicalContainer::PrimeIterator prime_iter4 = prime_iter1.begin();
+    cout << "*prime_iter4: " << *prime_iter4 << endl;
+    //end
+    MagicalContainer::PrimeIterator prime_iter5 = prime_iter4.end();
+    //should be true - 1
+    cout << "(prime_iter4 != prime_iter5): " << (prime_iter4 != prime_iter5) << endl;
+
     return 0;
 }
